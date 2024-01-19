64fa90aeba391ed9dbf0f9156f3e6a29e783a384
Ralph Vargas <ralphvargassorm@gmail.com>
2024-01-19 18:00:05 -0500

    added chat history

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const rl = readline.createInterface({ input, output });
 
 const createYoutubeVideoStore = async (videoLink) => {
-  console.log(videoLink);
   const loader = YoutubeLoader.createFromUrl(videoLink, {
     language: "en",
     addVideoInfo: true,
@@ -35,45 +34,53 @@ const createYoutubeVideoStore = async (videoLink) => {
 };
 
 const startQA = async (store) => {
-  const userQuestion = await rl.question("You: ");
+  const chatHistory = [
+    {
+      role: "assistant",
+      content:
+        "You are a helpful AI assistant. Answser questions to your best ability.",
+    },
+  ];
+  const userQuestion = await rl.question("\nYou: ");
   if (userQuestion.trim === "" || userQuestion.toLowerCase() === "exit") {
     rl.close();
     return;
   }
   const results = await store.similaritySearch(userQuestion, 2);
 
+  const newQuestion = {
+    role: "user",
+    content: `Answer the following question using the provided context. If you cannot answer the question with the context, don't lie and make up stuff. Just say you need more context.
+          Question: ${userQuestion}
+
+          Context: ${results.map((r) => r.pageContent).join("\n")}`,
+  };
+
+  chatHistory.push(newQuestion);
+
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     temperature: 0,
-    messages: [
-      {
-        role: "assistant",
-        content:
-          "You are a helpful AI assistant. Answser questions to your best ability.",
-      },
-      {
-        role: "user",
-        content: `Answer the following question using the provided context. If you cannot answer the question with the context, don't lie and make up stuff. Just say you need more context.
-              Question: ${userQuestion}
-
-              Context: ${results.map((r) => r.pageContent).join("\n")}`,
-      },
-    ],
+    messages: chatHistory,
   });
 
-  console.log(`AI: ${response.choices[0].message.content}`);
+  console.log(`\nAI: ${response.choices[0].message.content}`);
+  startQA(store);
 };
 
 const main = async () => {
-  console.log(
-    "Paste a YouTube link and then ask me any questions about the YouTube video"
-  );
+  console.log("-----------------------------------------------");
+  console.log("Welcome! Please paste a YouTube link to begin.");
+  console.log("Feel free to ask any questions about the video.");
+  console.log("-----------------------------------------------");
 
   const youTubeLink = await rl.question("YouTube Link: ");
+
   if (youTubeLink.trim() === "" || youTubeLink.toLowerCase() === "exit") {
     rl.close();
     return;
   }
+
   const store = await createYoutubeVideoStore(youTubeLink);
 
   startQA(store);
